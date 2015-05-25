1f9d549e58e13eecc25d4d9e7e0df2e09679aa5d
Alan Cowap <spam@alancowap.com>
2015-05-25 15:20:07 +0100

    Add Calculator class tweak layout of HelloWorld

---
+++ CaG2/src/com/alancowap/cag/cmd/Calculator.java
@@ -0,0 +1,67 @@
+/** 
+ * Menu driven program to be run from the Command line
+ *  Demonstrates I/O with user, if statements, basic operators: + - * /
+ *  Can be improved to use if - else-if
+ *	 
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies none
+ *  
+ *  
+ */
+
+package com.alancowap.cag.cmd;
+
+//menu driven program giving user choice of numerical operators
+//Demonstrates Console IO, numerical operators, if statement
+
+import java.io.Console;
+
+class Calculator{
+
+	public static void main(String[] args){
+		//Present Menu
+		System.out.println("~~~~~ Welcome to Calculator ~~~~~");
+		System.out.println("1) Addition");
+		System.out.println("2) Subtraction");
+		System.out.println("3) Multiplication");
+		System.out.println("4) Division");
+
+		//Display numbers
+		int num1 = 100;
+		int num2 = 10;
+		System.out.println("Num1 is: " +num1 + " Num2 is:" + num2);
+
+		//Get user choice
+		Console con = System.console();
+		String choice = con.readLine("Please enter a choice: ");		
+		System.out.println("You chose " + choice);
+		int selection = Integer.parseInt(choice);
+		
+
+		//Evaluate user choice
+		if(selection == 1){
+			System.out.println(num1 + num2);
+		}
+		if(selection == 2){
+			System.out.println(num1 - num2);
+		}
+		if(selection == 3){
+			System.out.println(num1 * num2);
+		}
+		if(selection == 4){
+			System.out.println(num1 / num2);
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
--- CaG2/src/com/alancowap/cag/cmd/HelloWorld.java
+++ CaG2/src/com/alancowap/cag/cmd/HelloWorld.java
@@ -7,25 +7,26 @@
  * @dependencies none
  *  
  *  
- */ package com.alancowap.cag.cmd;
+ */
+package com.alancowap.cag.cmd;
 
- import java.io.Console;
+import java.io.Console;
 
- public class HelloWorld {
+public class HelloWorld {
 
-	 public HelloWorld() {
-		 //Get number of loops from user
-		 Console con = System.console();
-		 String choice = con.readLine("Please enter number of loops: ");
+	public HelloWorld() {
+		//Get number of loops from user
+		Console con = System.console();
+		String choice = con.readLine("Please enter number of loops: ");
 
-		 //Convert String to int
-		 int numLoops = 0;
-		 numLoops = Integer.parseInt(choice);
+		//Convert String to int
+		int numLoops = 0;
+		numLoops = Integer.parseInt(choice);
 
-		 //Print name
-		 for(int i=0; i < numLoops; ++i){
-			 System.out.println(i+1 + ") Alan Cowap");
-		 }
-	 }
+		//Print name
+		for(int i=0; i < numLoops; ++i){
+			System.out.println(i+1 + ") Alan Cowap");
+		}
+	}
 
- }
+}
