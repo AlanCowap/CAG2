147b6bcfa442f4b2d5f27a2454304ed45ed73f3a
Alan Cowap <spam@alancowap.com>
2015-05-25 15:07:23 +0100

    Initial commit

---
+++ CaG2/src/com/alancowap/cag/cmd/HelloWorld.java
@@ -0,0 +1,31 @@
+/** 
+ * Sample first class
+ *  
+ *	 
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies none
+ *  
+ *  
+ */ package com.alancowap.cag.cmd;
+
+ import java.io.Console;
+
+ public class HelloWorld {
+
+	 public HelloWorld() {
+		 //Get number of loops from user
+		 Console con = System.console();
+		 String choice = con.readLine("Please enter number of loops: ");
+
+		 //Convert String to int
+		 int numLoops = 0;
+		 numLoops = Integer.parseInt(choice);
+
+		 //Print name
+		 for(int i=0; i < numLoops; ++i){
+			 System.out.println(i+1 + ") Alan Cowap");
+		 }
+	 }
+
+ }
