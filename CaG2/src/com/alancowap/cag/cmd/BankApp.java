905e27495da051e0e7ce65d662b92300fb54221e
Alan Cowap <spam@alancowap.com>
2015-05-27 14:14:44 +0100

    Add BankApp

--- CaG2/src/com/alancowap/cag/cmd/BankApp.java
+++ CaG2/src/com/alancowap/cag/cmd/BankApp.java
@@ -27,29 +27,38 @@ class BankApp{
 		//System.out.println("Chosen interest rate is: " + rate + "%");
 
 		// Calculate values
-		int interestEarned = principal * rate / 100;
+		int interestEarned = (principal * rate) / 100; //if possible multiply first, divide last to reduce rounding errors
 		int total = principal + interestEarned;
 		
 		// Print out results
 		System.out.println("Interest earned: EURO " + interestEarned);
 		System.out.println("Investment value: EURO " + total);
 		
-
 		// Integration and Testing
+		//Test with principal of 1000, and interest rate of 10%
+		//Test with principal of 999, and interest rate of 10% - what effect did the rounding have
+		
 	}
 
 }
 
 
+/*
+  PROBLEM DEFINITION
+  
+	Your team has been asked to write the BankApp application for a well known bank.
+	The BankApp application should allow an investment advisor to input an investment amount,
+	and an investment rate. The investment advisor should then be given the amount of the
+	interest earned, and the total value of the investment.
 
+ PROBLEM ANALYSIS
 
+	Inputs:
+	investment amount
+	investment rate
+	
+	Outputs:
+	amount of interest earned
+	total investment value
 
-//Inputs
-//investment amount
-//investment rate
-
-//Outputs
-//amount of interest earned
-//total investment value
-
-
+ */
