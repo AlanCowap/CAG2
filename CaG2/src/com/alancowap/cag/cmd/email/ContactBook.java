0fe0906b9b2e37cac553c2949a23da85b943e8e4
Alan Cowap <spam@alancowap.com>
2015-05-29 11:35:32 +0100

    Add getters and setters to ContactBook

--- CaG2/src/com/alancowap/cag/cmd/email/ContactBook.java
+++ CaG2/src/com/alancowap/cag/cmd/email/ContactBook.java
@@ -39,6 +39,50 @@ public class ContactBook {
 		this.contact3 = contact3;
 	}
 
+	
 	//Behaviour
+	
+	/**
+	 * @return the contact1
+	 */
+	public Contact getContact1() {
+		return contact1;
+	}
+
+	/**
+	 * @param contact1 the contact1 to set
+	 */
+	public void setContact1(Contact contact1) {
+		this.contact1 = contact1;
+	}
+
+	/**
+	 * @return the contact2
+	 */
+	public Contact getContact2() {
+		return contact2;
+	}
+
+	/**
+	 * @param contact2 the contact2 to set
+	 */
+	public void setContact2(Contact contact2) {
+		this.contact2 = contact2;
+	}
+
+	/**
+	 * @return the contact3
+	 */
+	public Contact getContact3() {
+		return contact3;
+	}
+
+	/**
+	 * @param contact3 the contact3 to set
+	 */
+	public void setContact3(Contact contact3) {
+		this.contact3 = contact3;
+	}
+
 	
 }
