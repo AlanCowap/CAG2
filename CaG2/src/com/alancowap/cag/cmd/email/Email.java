cbf2f3ac55989b687f85438c42a5ccb9f8ac9ffe
Alan Cowap <spam@alancowap.com>
2015-05-29 11:04:22 +0100

    Add sample constructor

--- CaG2/src/com/alancowap/cag/cmd/email/Email.java
+++ CaG2/src/com/alancowap/cag/cmd/email/Email.java
@@ -1,6 +1,7 @@
 /** 
- *  Demo class definition
+ *  Demo class definition, with state and behaviour
  *  
+ *  Could be improved to allow multiple to, cc, and bcc
  *  
  * @author Alan Cowap 
  * @version 1.0  
@@ -12,8 +13,32 @@ package com.alancowap.cag.cmd.email;
 
 public class Email {
 
-	public Email() {
-		// TODO Auto-generated constructor stub
+	// state
+	private String from;
+	private String to;
+	private String cc;
+	private String bcc;
+	private String subject;
+	private String body;
+	
+	/**
+	 * Sample cosntructor to create a simple Email
+	 * This construcor is called with e.g. new Email("me@here.com", "you@there.com", "hi", "hello world")
+	 * 
+	 * @param from
+	 * @param to
+	 * @param subject
+	 * @param body
+	 */
+	public Email(String from, String to, String subject, String body) {
+		this.from = from;
+		this.to = to;
+		this.subject = subject;
+		this.body = body;
 	}
+	
+	
+	//Behaviour
+	
 
 }
