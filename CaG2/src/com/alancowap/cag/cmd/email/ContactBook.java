bc984feeb611bb6b4f0a07b9d60dbf5989339881
Alan Cowap <spam@alancowap.com>
2015-05-29 11:32:07 +0100

    Add ContactBook class

---
+++ CaG2/src/com/alancowap/cag/cmd/email/ContactBook.java
@@ -0,0 +1,33 @@
+/** 
+ *  Simple class to represent a ContactBook
+ *  
+ *  Demo class definition, with state and behaviour
+ *  Demo constructor, demo toString
+ *  This is a simple sample class :)
+ *  
+ *  Assume learner has no knowledge of data structures, or iteration statements
+ *  Simply allow 3 Contacts in the ContactBook
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ *  
+ */
+
+package com.alancowap.cag.cmd.email;
+
+public class ContactBook {
+
+	//State
+	Contact contact1;
+	Contact contact2;
+	Contact contact3;
+	
+	public ContactBook() {
+		// TODO Auto-generated constructor stub
+	}
+
+	//Behaviour
+	
+}
