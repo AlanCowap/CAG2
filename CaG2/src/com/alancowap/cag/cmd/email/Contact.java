222fb486f602d97158d43dfc292122771d42fb67
Alan Cowap <spam@alancowap.com>
2015-05-29 11:22:25 +0100

    Add getters and setters to Contact

--- CaG2/src/com/alancowap/cag/cmd/email/Contact.java
+++ CaG2/src/com/alancowap/cag/cmd/email/Contact.java
@@ -34,7 +34,55 @@ public class Contact {
 		this.phoneNumber = phoneNumber;
 	}
 
-	//Behaviour
 	
+	//Behaviour	
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
 	
 }
