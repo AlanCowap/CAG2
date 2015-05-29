06c4b5f642de908863291361ba8717876fe9a46b
Alan Cowap <spam@alancowap.com>
2015-05-29 10:43:08 +0100

    Initial commit

---
+++ CaG2/src/com/alancowap/cag/cmd/email/Email.java
@@ -0,0 +1,19 @@
+/** 
+ *  Demo class definition
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ *  
+ */
+package com.alancowap.cag.cmd.email;
+
+public class Email {
+
+	public Email() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
