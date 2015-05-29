74bed7f0b7a9c9671355952617e98d95dedaa1d9
Alan Cowap <spam@alancowap.com>
2015-05-29 11:09:48 +0100

    add getters and setters

--- CaG2/src/com/alancowap/cag/cmd/email/Email.java
+++ CaG2/src/com/alancowap/cag/cmd/email/Email.java
@@ -40,5 +40,90 @@ public class Email {
 	
 	//Behaviour
 	
+	
+	/**
+	 * @return the from
+	 */
+	public String getFrom() {
+		return from;
+	}
+
+
+	/**
+	 * @param from the from to set
+	 */
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	/**
+	 * @return the to
+	 */
+	public String getTo() {
+		return to;
+	}
+
+	/**
+	 * @param to the to to set
+	 */
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	/**
+	 * @return the cc
+	 */
+	public String getCc() {
+		return cc;
+	}
+
+	/**
+	 * @param cc the cc to set
+	 */
+	public void setCc(String cc) {
+		this.cc = cc;
+	}
+
+	/**
+	 * @return the bcc
+	 */
+	public String getBcc() {
+		return bcc;
+	}
+
+	/**
+	 * @param bcc the bcc to set
+	 */
+	public void setBcc(String bcc) {
+		this.bcc = bcc;
+	}
+
+	/**
+	 * @return the subject
+	 */
+	public String getSubject() {
+		return subject;
+	}
+
+	/**
+	 * @param subject the subject to set
+	 */
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	/**
+	 * @return the body
+	 */
+	public String getBody() {
+		return body;
+	}
+
+	/**
+	 * @param body the body to set
+	 */
+	public void setBody(String body) {
+		this.body = body;
+	}
 
 }
