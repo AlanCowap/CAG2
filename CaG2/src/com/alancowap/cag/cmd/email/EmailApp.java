0846460a7abc7150c3f7d9b7160be5712bf4ee04
Alan Cowap <spam@alancowap.com>
2015-05-29 11:45:06 +0100

    Add EmailApp

---
+++ CaG2/src/com/alancowap/cag/cmd/email/EmailApp.java
@@ -0,0 +1,27 @@
+/** 
+ *  Simple class to represent a ContactBook
+ *  
+ *  Demo class definition, with state and behaviour
+ *  Demo constructor, demo toString
+ *  This is a simple sample class :)
+ *  
+ *  Assume learner has no knowledge of data structures, or iteration statements
+ *  Simply allow 3 Contacts in the ContactBook
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ *  
+ */
+package com.alancowap.cag.cmd.email;
+
+public class EmailApp {
+
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmailApp");
+
+	}
+
+}
