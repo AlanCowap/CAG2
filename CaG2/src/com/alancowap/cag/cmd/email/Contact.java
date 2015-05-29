d171fca1f7042345206fe7c519ac8433637390a9
Alan Cowap <spam@alancowap.com>
2015-05-29 11:13:55 +0100

    Add Contact

---
+++ CaG2/src/com/alancowap/cag/cmd/email/Contact.java
@@ -0,0 +1,20 @@
+/** 
+ *  Demo class definition, with state and behaviour
+ *  
+ *  
+ *  
+ * @author Alan Cowap 
+ * @version 1.0  
+ * @dependencies None
+ *  
+ *  
+ */
+package com.alancowap.cag.cmd.email;
+
+public class Contact {
+
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
