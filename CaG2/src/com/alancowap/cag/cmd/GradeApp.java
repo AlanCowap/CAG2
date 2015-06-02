0bdc12054c0049019457f0e1331256294a7012c1
Alan Cowap <spam@alancowap.com>
2015-06-02 11:07:02 +0100

    Add switch statement

--- CaG2/src/com/alancowap/cag/cmd/GradeApp.java
+++ CaG2/src/com/alancowap/cag/cmd/GradeApp.java
@@ -18,7 +18,7 @@ class GradeApp{
 		// get score
 		int score = 88;
 		char grade = 'Z';
-		
+
 		// compare score & output grade
 		if (score > 84){
 			grade = 'A';
@@ -36,5 +36,18 @@ class GradeApp{
 
 		//Output Grade
 		System.out.println("You got an " + grade);
+
+		// Use switch statement to convert grade to score range
+		String result = "Not set";
+		switch (grade){
+			case 'A' : result = "85-100"; break;
+			case 'B' : result = "70-84"; break;
+			case 'C' : result = "55-69"; break;
+			case 'D' : result = "40-54"; break;
+			default  : result = "Fail"; break;
+		}
+		System.out.println(result);
+
 	}
+
 }
