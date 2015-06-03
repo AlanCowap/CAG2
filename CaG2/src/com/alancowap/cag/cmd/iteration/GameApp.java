529d49d63f6764c57cfdc30a8537b744dd3e3ad8
Alan Cowap <spam@alancowap.com>
2015-06-03 11:28:16 +0100

    Demo while loop

---
+++ CaG2/src/com/alancowap/cag/cmd/iteration/GameApp.java
@@ -0,0 +1,25 @@
+/** 
+ *  Demo iteration statement (while loop)
+ *  
+ *	 
+ * @author Alan Cowap 
+ * @version 1.1  
+ * @dependencies none
+ *  
+ *  
+ */
+
+package com.alancowap.cag.cmd.iteration;
+
+public class GameApp {
+
+	public static void main(String[] args) {
+		int numLives = 4;
+		while( numLives != 0 ){
+			System.out.println("Number of lives remaining: " + numLives);
+			numLives--;	
+		}
+		System.out.println("GAME OVER");		
+	}
+
+}
