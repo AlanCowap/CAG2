887fae042d106510d86e00b025263c33bb23c529
Alan Cowap <spam@alancowap.com>
2015-06-03 10:07:16 +0100

    Initial commit

---
+++ CaG2/src/com/alancowap/cag/cmd/iteration/NameApp.java
@@ -0,0 +1,29 @@
+/** 
+ *  Demo iteration statement (for loop)
+ *  
+ *	 
+ * @author Alan Cowap 
+ * @version 1.1  
+ * @dependencies none
+ *  
+ *  
+ */
+
+package com.alancowap.cag.cmd.iteration;
+
+public class NameApp {
+
+	public static void main(String[] args) {
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+		System.out.println("Alan C");
+	}
+
+}
